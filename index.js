2dea2fa87ffd4cb6bc37756f4bd2fd8ba860d2f8
Wiztech-01 <lynnnyaoke@gmail.com>
2023-10-13 11:49:10 +0300

    GET to fetch data

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-document.addEventListener
-("DOMContentLoaded",(e) => {
-    // console.log("loaded")
-    fetch("https://rich-pink-crane-sock.cyclic.app/meals")
-    .then(res => res.json())
-    .then(data => console.log(data))
-    data.forEach((recipe) => {
-        
+document.addEventListener("DOMContentLoaded", (e) => {
+    e.preventDefault();
+  
+    const foodList = document.querySelector(".food-list");
+    const recipeImage = document.querySelector(".recipe-image");
+  
+    // Function to fetch meal data
+    function fetchMealData() {
+        fetch("http://localhost:3000/meals")
+            .then((response) => response.json())
+            .then((data) => {
+                if (Array.isArray(data)) {
+                    data.forEach((meal) => {
+                        const mealItem = document.createElement("li");
+                        mealItem.textContent = meal.strMeal;
+  
+                    // Add a click event listener to display recipe details
+                    mealItem.addEventListener("click", () => {
+                        // Display the clicked meal details in the recipe-image div
+                        const mealDetails = `
+                            <h2>${meal.strMeal}</h2>
+                            <p>${meal.strInstructions}</p>
+                            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" style="width: 300px; height: 200px;">
+                        `;
+                        recipeImage.innerHTML = mealDetails;
+                    });
+  
+                    // Append the meal item to the food list
+                    foodList.appendChild(mealItem);
+                });
+            } else {
+                console.error("No meals data found.");
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching meal data:", error);
+        });
     }
-    )
-})
+  
+    // Call the function to fetch and display meal data
+    fetchMealData();
+  });
+  
+  
